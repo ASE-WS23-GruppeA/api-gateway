2b6a55df844b4a45b15abb3bae683af16dfe4aec
Timo Tabertshofer <timo.tabertshofer@gmail.com>
2024-01-19 18:27:47 +0100

    Cleanup protected routes in SecurityConfig

--- src/main/java/at/aau/ase/workouttrackerapigateway/SecurityConfig.java
+++ src/main/java/at/aau/ase/workouttrackerapigateway/SecurityConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
@@ -17,21 +16,13 @@ public class SecurityConfig {
   SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
     http
         .csrf(ServerHttpSecurity.CsrfSpec::disable)
-        .authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec
-            // TODO (Timo Tabertshofer, 02.11.2023): remove unused routes
-            .pathMatchers("/headerrouting/**").permitAll()
+        .authorizeExchange(exchangeSpec -> exchangeSpec
             .pathMatchers("/actuator/**").permitAll()
-            .pathMatchers("/eureka/**").permitAll()
-            .pathMatchers("/oauth2/**").permitAll()
-            .pathMatchers("/login/**").permitAll()
             .pathMatchers("/api/auth/login").permitAll()
             .pathMatchers("/api/auth/register").permitAll()
-            .pathMatchers("/error/**").permitAll()
-            .pathMatchers("/openapi/**").permitAll()
-            .pathMatchers("/webjars/**").permitAll()
             .anyExchange().authenticated()
         )
-        .oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec
+        .oauth2ResourceServer(resourceServerSpec -> resourceServerSpec
             .jwt(withDefaults())
         );
 
